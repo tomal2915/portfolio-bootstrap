19e43fcf986e92223fe9e1c19700825158281ef8
Tomal <57248708+tomal2915@users.noreply.github.com>
2022-02-19 17:19:07 +0600

    practice

---
+++ nav.js
@@ -0,0 +1,19 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+// $(function() {
+//     $('.social a').tooltip({
+//       placement: "right",
+//       container: 'body'
+//     });
+//   });
